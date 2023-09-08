02ae67189c181b0d35a13b21f332fa972d6d4002
Vitor de Miranda Almagro <vitor.almagro@toroinvestimentos.com.br>
2023-09-08 09:10:01 -0300

    FEAT: Adicionando testes unitarios

---
+++ ChallengeDotNet.Test.Unit/OrquestradorDeCalculoTests.cs
@@ -0,0 +1,17 @@
+using Xunit;
+
+namespace ChallengeDotNet.Test.Unit;
+
+public class OrquestradorDeCalculoTests
+{
+    public OrquestradorDeCalculoTests()
+    {
+        
+    }
+    
+    [Fact]
+    public void CalcularENotificar_Should_When()
+    {
+        
+    }
+}
